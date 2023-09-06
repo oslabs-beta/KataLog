b4ece9b3718224baf7df19aa6dac436c425a77b6
Thomas Pappas <thomaskpappas@gmail.com>
2023-09-06 10:12:55 -0400

    frontend adjustments, dialog box styling for future filter functionality, preliminary pagination

---
+++ src/components/Filter.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog, { DialogProps } from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import BuildIcon from '@mui/icons-material/Build';
+import ListItemButton from '@mui/material/ListItemButton';
+import MultipleSelectChip from './Chip';
+
+export default function ScrollDialog() {
+
+  const [open, setOpen] = React.useState(false);
+  const [scroll, setScroll] = React.useState<DialogProps['scroll']>('paper');
+
+  const handleClickOpen = (scrollType: DialogProps['scroll']) => () => {
+    setOpen(true);
+    setScroll(scrollType);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const descriptionElementRef = React.useRef<HTMLElement>(null);
+  React.useEffect(() => {
+    if (open) {
+      const { current: descriptionElement } = descriptionElementRef;
+      if (descriptionElement !== null) {
+        descriptionElement.focus();
+      }
+    }
+  }, [open]);
+
+  return (
+    <div>
+      {/* <Button onClick={handleClickOpen('paper')}>scroll=paper</Button> */}
+      <ListItemButton onClick={handleClickOpen('paper')}>
+        <BuildIcon />
+      </ListItemButton>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        scroll={scroll}
+        aria-labelledby="scroll-dialog-title"
+        aria-describedby="scroll-dialog-description"
+      >
+        <DialogTitle id="scroll-dialog-title">Filter Logs</DialogTitle>
+        <DialogContent dividers={scroll === 'paper'}>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            <MultipleSelectChip/>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -6,12 +6,21 @@ import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import BuildIcon from '@mui/icons-material/Build';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
 import Title from './Title';
+import ScrollDialog from './Filter';
+import { TextField, Box, Grid, Typography } from '@mui/material';
 
 
 function preventDefault(event: React.MouseEvent) {
   event.preventDefault();
 }
+
+const itemsPerPage = 10;
 
 export default function Orders() {
 
@@ -20,16 +29,25 @@ export default function Orders() {
     timestamp: string;
     sourceInfo: string;
     logObject: LogObject;
+    podInfo: PodObject;
+    type: string;
   }
   
   interface LogObject {
     log: string;
     stream: string;
   }
+
+  interface PodObject {
+    containerName: string;
+    namespace: string;
+    podName: string;
+  }
   
   const initialLogData: Log[] = [];
   
     const [logData, setLogData] = useState<Log[]>(initialLogData);
+    const [currentPage, setCurrentPage] = useState(1);
   
     useEffect(() => {
       fetch('/api/logs')
@@ -40,35 +58,80 @@ export default function Orders() {
           return response.json() as Promise<Log[]>; // Specify the response type as an array of Log
         })
         .then((data) => {
+          // console.log(data);
           const newData = data.slice(0, data.length - 1);
           setLogData(newData); // Use data directly if it's an array
         })
         .catch((err) => console.error('An error occurred in getting logs: ', err));
     }, []);
 
+    const totalPages = Math.ceil(logData.length / itemsPerPage);
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentLogData = logData.slice(startIndex, endIndex);
 
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
 
   return (
     <React.Fragment>
-  <Title>Your Centralized Kubernetes Logs:</Title>
+      <Box display="flex" alignItems="center">
+        {/* <Typography variant="h6">Your Centralized Kubernetes Logs:</Typography> */}
+        <Title>Your Centralized Kubernetes Logs: </Title>
+        <Grid container alignItems="center" justifyContent="flex-end">
+          <Grid item>
+            <TextField id="standard-basic" label="Search" variant="standard" sx={{ width: '300px' }}/>
+          </Grid>
+        </Grid>
+          <Grid item>
+              <ScrollDialog />
+          </Grid>
+      </Box>
   <Table size="small">
     <TableHead>
       <TableRow>
         <TableCell>Time Stamp</TableCell>
+        <TableCell>Type</TableCell>
         <TableCell>Source</TableCell>
-        <TableCell align="left">Log</TableCell>
+        <TableCell>Namespace</TableCell>
+        <TableCell>Container</TableCell>
+        <TableCell>Pod</TableCell>
+        <TableCell>Log</TableCell>
+        <TableCell align="left">Stream</TableCell>
       </TableRow>
     </TableHead>
     <TableBody>
-      {logData.map((row) => (
-        <TableRow key={row.timestamp}>
+      {logData.map((row, index) => (
+        <TableRow key={index}>
           <TableCell>{row.timestamp}</TableCell>
+          <TableCell>{row.type}</TableCell>
           <TableCell>{row.sourceInfo}</TableCell>
-          <TableCell align="left" >{row.logObject.log}</TableCell>
+          <TableCell>{row.podInfo.namespace}</TableCell>
+          <TableCell>{row.podInfo.containerName}</TableCell>
+          <TableCell>{row.podInfo.podName}</TableCell>
+          <TableCell>{row.logObject.log}</TableCell>
+          <TableCell align="left" >{row.logObject.stream}</TableCell>
         </TableRow>
       ))}
     </TableBody>
   </Table>
+  {/* Pagination controls */}
+  <Box display="flex" justifyContent="center" alignItems="center" mt={2}>
+        {Array.from({ length: totalPages }, (_, i) => (
+          <Link
+            key={i}
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              handlePageChange(i + 1);
+            }}
+            style={{ padding: '4px', cursor: 'pointer', fontWeight: i + 1 === currentPage ? 'bold' : 'normal' }}
+          >
+            {i + 1}
+          </Link>
+        ))}
+      </Box>
   <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
     See more Logs
   </Link>
