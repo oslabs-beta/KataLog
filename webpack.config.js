3b46b4a041503d4ffc781b24dc8adea38c6735bc
Derrick Devairakkam <derrickdaivairakkam@gmail.com>
2023-09-07 18:46:42 -0400

    added sockjs to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 // const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/App.tsx',
+  mode: "development",
+  entry: "./src/App.tsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         exclude: /node_modules/,
-        use: ['ts-loader'],
+        use: ["ts-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
+      template: "./src/index.html",
+      filename: "./index.html",
     }),
     // new CopyPlugin({
     //   patterns: [{ from: './src/client/style.css' }],
@@ -32,11 +32,18 @@ module.exports = {
   ],
   devServer: {
     static: {
-      directory: path.join(__dirname, './dist'),
+      directory: path.join(__dirname, "./dist"),
     },
+    client: { webSocketTransport: "sockjs" },
+    webSocketServer: "sockjs",
     proxy: {
-      '/api': 'http://localhost:3000',
-      secure: false
-    }
+      "/api": {
+        target: "http://localhost:3000",
+      },
+    },
+    // proxy: {
+    //   "/api": "http://localhost:3000",
+    //   secure: false,
+    // },
   },
-}
+};
