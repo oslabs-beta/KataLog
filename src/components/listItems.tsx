0f4963d78654d995f95ab42036682d154017acf4
Derrick Devairakkam <derrickdaivairakkam@gmail.com>
2023-10-11 15:50:32 -0400

    finished functionality, need styling

--- src/components/listItems.tsx
+++ src/components/listItems.tsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
+// import ListSubheader from '@mui/material/ListSubheader';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 // import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import PeopleIcon from '@mui/icons-material/People';
 import BarChartIcon from '@mui/icons-material/BarChart';
 import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
+// import AssignmentIcon from '@mui/icons-material/Assignment';
 import GitHubIcon from '@mui/icons-material/GitHub';
 
 export const mainListItems = (
@@ -18,7 +18,9 @@ export const mainListItems = (
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
-      <ListItemText primary="Dashboard" />
+      <Link to='/'>
+        <ListItemText primary="Dashboard" />
+      </Link>
     </ListItemButton>
 
   <a href="https://github.com/oslabs-beta/Katalog"
