2700901a94f4e2f40b5ec77473a977ff1f6ffdeb
Derrick Devairakkam <derrickdaivairakkam@gmail.com>
2023-10-16 20:46:45 -0400

    fixed search bug and pagination

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -8,9 +8,10 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import ListItemButton from '@mui/material/ListItemButton';
 import ScrollDialog from './Filter';
-import { TextField, Box, Grid, Typography } from '@mui/material';
+import { TextField, Box, Grid, Typography, Pagination, Stack, Paper } from '@mui/material';
 import FilterAltOffIcon from '@mui/icons-material/FilterAltOff';
 import NestedRowData from './NestedRowData';
+
 
 
 function preventDefault(event: React.MouseEvent) {
@@ -39,11 +40,16 @@ export default function Orders(props) {
   }
 
   const initialLogData: Log[] = [];
-
+  const itemsPerPage = 15; // Number of items to display per page
+  const [page, setPage] = useState(1);
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
   // const [logData, setLogData] = useState<Log[]>(initialLogData);
   const [filteredLogs, setFilteredLogs] = useState<Log[]>(initialLogData);
   const [searchLogs, setSearchLogs] = useState<Log[]>(initialLogData);
   const [filterTypes, setFilterTypes] = useState<string[]>([]);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+
   // const [expanded, setExpanded] = useState(false);
 
   // const toggleExpanded = () => {
@@ -54,17 +60,20 @@ export default function Orders(props) {
   // function to handle the logic of filtering logs
   const handleFilterLogs = () => {
     // iterate over logData
-      // on each iteration, check to see if the type of the current index matches anything within filterTypes
-        // if yes, then push to filteredLogs (don't actually push, just update state at the end)
-        // if logdata includes filtertypes
+    // on each iteration, check to see if the type of the current index matches anything within filterTypes
+    // if yes, then push to filteredLogs (don't actually push, just update state at the end)
+    // if logdata includes filtertypes
     setFilteredLogs(props.logData.filter((log) => filterTypes.includes(log.type)));
   };
+  // console.log(displayedItems, startIndex, endIndex)
 
-  const handleSearchLogs = (e) => {
+  const handleSearchLogs = (e: { target: { value: string; }; }) => {
 
-    const searchTerm : string = e.target.value;
+    setSearchTerm(e.target.value);
 
     const temp : Log[] = [];
+
+    // search filtered logs if they exist, otherwise search through all logs. (passed in log data)
     const logsToSearch = filteredLogs.length ? filteredLogs : props.logData;
 
       for (let i = 0; i < logsToSearch.length; i++) {
@@ -92,11 +101,14 @@ export default function Orders(props) {
       }
       setSearchLogs(temp);
     }
-
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+    }
   const handleClearFilter = () => {
     setFilteredLogs([]);
     setFilterTypes([]);
     setSearchLogs([]);
+    setSearchTerm('');
     let text : any = document.getElementById("standard-basic");
     text.value = '';
   };
@@ -110,14 +122,21 @@ export default function Orders(props) {
   //       return response.json() as Promise<Log[]>; // Specify the response type as an array of Log
   //     })
   //     .then((data) => {
-  //       // console.log(data);
-  //       const newData = data.slice(0, data.length - 1);
-  //       console.log(newData);
-  //       setLogData(newData); // Use data directly if it's an array
-  //     })
-  //     .catch((err) => console.error('An error occurred in getting logs: ', err));
-  // }, []);
+    //       // console.log(data);
+    //       const newData = data.slice(0, data.length - 1);
+    //       console.log(newData);
+    //       setLogData(newData); // Use data directly if it's an array
+    //     })
+    //     .catch((err) => console.error('An error occurred in getting logs: ', err));
+    // }, []);
 
+    // if there is search text, set returned logs to be search logs.
+    // if there is a filter and no search logs, set returned logs to be filtered logs.
+    // if theres neither, set returned logs to be all logs.
+    const returnedLogs = searchTerm ? searchLogs :
+                          filterTypes.length > 0 && !searchTerm ? filteredLogs : props.logData;
+                          // filterTypes.length === 0 && text ? searchLogs : props.logData;
+    const displayedLogs = returnedLogs.slice(startIndex, endIndex);
   return (
     <React.Fragment>
       <Box display="flex" alignItems="center">
@@ -168,24 +187,13 @@ export default function Orders(props) {
         </TableRow>
       </TableHead>
       <TableBody>
-      {!filteredLogs.length && !searchLogs.length && (
-        <>
-        {props.logData.map((row, index) => (
-          <NestedRowData row={row} />
-        ))}
-        </>
-      )}
+          <>
+          {displayedLogs.map((row, index) => (
+            <NestedRowData row={row} />
+          ))}
+          </>
       </TableBody>
-        <TableBody>
-      {filteredLogs.length && !searchLogs.length && (
-        <>
-        {filteredLogs.map((row, index) => (
-          <NestedRowData row={row} />
-        ))}
-        </>
-      )}
-      </TableBody>
-      <TableBody>
+      {/* <TableBody>
       {searchLogs.length && (
         <>
         {searchLogs.map((row, index) => (
@@ -194,9 +202,28 @@ export default function Orders(props) {
         </>
       )}
       </TableBody>
+      <TableBody>
+      {(
+        <>
+        {props.logData.map((row, index) => (
+          <NestedRowData row={row} />
+        ))}
+        </>
+      )}
+      </TableBody> */}
     </Table>
     <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
       See more Logs
     </Link>
+    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column', backgroundColor: '#424242' }}>
+      <Stack spacing={2} sx={{ justifyContent: 'center' }}>
+          <Pagination
+            count={Math.ceil(returnedLogs.length / itemsPerPage)} // set this according to search type
+            page={page}
+            onChange={handleChangePage}
+            color="primary"
+          />
+      </Stack>
+    </Paper>
   </React.Fragment>
 )}
