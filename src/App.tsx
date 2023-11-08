3ea40ba50bfadf60fb24e6d3eb15465abb5ad97b
Thomas Pappas <thomaskpappas@gmail.com>
2023-11-07 22:46:05 -0500

    frontend splash page

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Homepage from './pages/Homepage';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Projects from './pages/Projects';
 import Dashboard from './pages/Dashboard';
 import FluentdConfigGenerator from './pages/FluentdConfigGenerator';
 import Logs from './pages/Logs';
+import SplashPage from './pages/SplashPage';
 
 const App = () => {
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Dashboard/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
-          <Route path='/signup' element={<Signup/>}></Route>
-          <Route path='/projects' element={<Projects/>}></Route>
-          <Route path='/homepage' element={<Homepage/>}></Route>
-          <Route path='/configuration' element={<FluentdConfigGenerator/>}></Route>
-          <Route path='/api/logs' element={<Logs/>}></Route>
-        </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+          <Routes>
+            <Route path='/dashboard' element={<Dashboard/>}></Route>
+            <Route path='/login' element={<Login/>}></Route>
+            <Route path='/signup' element={<Signup/>}></Route>
+            <Route path='/projects' element={<Projects/>}></Route>
+            <Route path='/configuration' element={<FluentdConfigGenerator/>}></Route>
+            <Route path='/api/logs' element={<Logs/>}></Route>
+            <Route path='/' element={<SplashPage/>}></Route>
+          </Routes>
+      </BrowserRouter>
   );
 };
 
