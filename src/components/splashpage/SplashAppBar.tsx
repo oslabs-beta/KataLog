8b15abd5acdffe9d6438a8f89698214090e8f2f0
Derrick Devairakkam <derrickdaivairakkam@gmail.com>
2023-11-09 17:31:57 -0500

    final graph changes

---
+++ src/components/splashpage/SplashAppBar.tsx
@@ -0,0 +1,317 @@
+'use client'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import test from '../assets/SmallLogo.png';
+
+import {
+  Box,
+  Flex,
+  Avatar,
+  Text,
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuDivider,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  useColorMode,
+  Center,
+  Image,
+  Icon,
+  Collapse,
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  useBreakpointValue,
+} from '@chakra-ui/react'
+import { MoonIcon, SunIcon } from '@chakra-ui/icons'
+
+import {
+    HamburgerIcon,
+    CloseIcon,
+    ChevronDownIcon,
+    ChevronRightIcon,
+  } from '@chakra-ui/icons'
+
+interface Props {
+  children: React.ReactNode
+}
+
+const NavLink = (props: Props) => {
+  const { children } = props
+
+  return (
+    <Box
+      as="a"
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{
+        textDecoration: 'none',
+        bg: useColorModeValue('gray.200', 'gray.700'),
+      }}
+      href={'#'}>
+      {children}
+    </Box>
+  )
+}
+
+export default function Nav() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  
+  const navigate = useNavigate();
+  
+  const handleSignup = () => {
+      navigate('/signup');
+    }
+    
+    const handleLogin = () => {
+        navigate('/login');
+    } 
+    
+    return (
+        <>
+      <Box bg={useColorModeValue('gray.100', 'gray.900')} px={5}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <Image
+                  w={75}
+                  h={75}
+                  rounded={'md'}
+                  alt={'feature image'}
+                  src={
+                    test
+                  }
+                  objectFit={'cover'}
+                />
+        <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
+          {/* <Text
+            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
+            fontFamily={'heading'}
+            color={useColorModeValue('gray.800', 'white')}>
+            Logo
+          </Text> */}
+
+          <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
+            <DesktopNav />
+          </Flex>
+        </Flex>
+
+
+          <Flex alignItems={'center'}>
+          <Stack
+          flex={{ base: 1, md: 0 }}
+          justify={'flex-end'}
+          direction={'row'}
+          spacing={6}>
+          <Button as={'a'} fontSize={'sm'} fontWeight={400} variant={'link'} href={'#'} onClick={handleLogin}>
+            Log In
+          </Button>
+          <Button
+            as={'a'}
+            display={{ base: 'none', md: 'inline-flex' }}
+            fontSize={'sm'}
+            fontWeight={600}
+            color={'white'}
+            bg={"#316CE6"}
+            href={'#'}
+            onClick={handleSignup}
+            _hover={{
+                bg: 'gray.500',
+            }}>
+            Sign Up
+          </Button>
+        </Stack>
+
+            <Stack direction={'row'} spacing={7}>
+              <Button onClick={toggleColorMode}>
+                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+              </Button>
+
+            </Stack>
+          </Flex>
+        </Flex>
+      </Box>
+    </>
+  )
+}
+
+const DesktopNav = () => {
+    const linkColor = useColorModeValue('gray.600', 'gray.200')
+    const linkHoverColor = useColorModeValue('gray.800', 'white')
+    const popoverContentBgColor = useColorModeValue('white', 'gray.800')
+    
+    const handleScrollToSection = (ref) => {
+      if (ref.current) {
+        ref.current.scrollIntoView({ behavior: 'smooth' });
+      }
+    };
+
+    return (
+      <Stack direction={'row'} spacing={4}>
+        {NAV_ITEMS.map((navItem) => (
+          <Box key={navItem.label}>
+            <Popover trigger={'hover'} placement={'bottom-start'}>
+              <PopoverTrigger>
+                <Box
+                  as="a"
+                  p={2}
+                  href={navItem.href ?? '#'}
+                  fontSize={'sm'}
+                  fontWeight={500}
+                  color={linkColor}
+                  _hover={{
+                    textDecoration: 'none',
+                    color: linkHoverColor,
+                  }}>
+                  {navItem.label}
+                </Box>
+              </PopoverTrigger>
+  
+              {navItem.children && (
+                <PopoverContent
+                  border={0}
+                  boxShadow={'xl'}
+                  bg={popoverContentBgColor}
+                  p={4}
+                  rounded={'xl'}
+                  minW={'sm'}>
+                  <Stack>
+                    {navItem.children.map((child) => (
+                      <DesktopSubNav key={child.label} {...child} />
+                    ))}
+                  </Stack>
+                </PopoverContent>
+              )}
+            </Popover>
+          </Box>
+        ))}
+      </Stack>
+    )
+  }
+  
+  const DesktopSubNav = ({ label, href, subLabel }: NavItem) => {
+    return (
+      <Box
+        as="a"
+        href={href}
+        role={'group'}
+        display={'block'}
+        p={2}
+        rounded={'md'}
+        _hover={{ bg: useColorModeValue("#316CE6", 'gray.900') }}>
+        <Stack direction={'row'} align={'center'}>
+          <Box>
+            <Text
+              transition={'all .3s ease'}
+              _groupHover={{ color: "#316CE6" }}
+              fontWeight={500}>
+              {label}
+            </Text>
+            <Text fontSize={'sm'}>{subLabel}</Text>
+          </Box>
+          <Flex
+            transition={'all .3s ease'}
+            transform={'translateX(-10px)'}
+            opacity={0}
+            _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
+            justify={'flex-end'}
+            align={'center'}
+            flex={1}>
+            <Icon color={"#316CE6"} w={5} h={5} as={ChevronRightIcon} />
+          </Flex>
+        </Stack>
+      </Box>
+    )
+  }
+  
+  const MobileNav = () => {
+    return (
+      <Stack bg={useColorModeValue('white', 'gray.800')} p={4} display={{ md: 'none' }}>
+        {NAV_ITEMS.map((navItem) => (
+          <MobileNavItem key={navItem.label} {...navItem} />
+        ))}
+      </Stack>
+    )
+  }
+  
+  const MobileNavItem = ({ label, children, href }: NavItem) => {
+    const { isOpen, onToggle } = useDisclosure()
+  
+    return (
+      <Stack spacing={4} onClick={children && onToggle}>
+        <Box
+          py={2}
+          as="a"
+          href={href ?? '#'}
+          justifyContent="space-between"
+          alignItems="center"
+          _hover={{
+            textDecoration: 'none',
+          }}>
+          <Text fontWeight={600} color={useColorModeValue('gray.600', 'gray.200')}>
+            {label}
+          </Text>
+          {children && (
+            <Icon
+              as={ChevronDownIcon}
+              transition={'all .25s ease-in-out'}
+              transform={isOpen ? 'rotate(180deg)' : ''}
+              w={6}
+              h={6}
+            />
+          )}
+        </Box>
+  
+        <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
+          <Stack
+            mt={2}
+            pl={4}
+            borderLeft={1}
+            borderStyle={'solid'}
+            borderColor={useColorModeValue('gray.200', 'gray.700')}
+            align={'start'}>
+            {children &&
+              children.map((child) => (
+                <Box as="a" key={child.label} py={2} href={child.href}>
+                  {child.label}
+                </Box>
+              ))}
+          </Stack>
+        </Collapse>
+      </Stack>
+    )
+  }
+  
+  interface NavItem {
+    label: string
+    subLabel?: string
+    children?: Array<NavItem>
+    href?: string
+    target?: string
+  }
+  
+  const NAV_ITEMS: Array<NavItem> = [
+    {
+      label: 'Features',
+      href: '#',
+    },
+    {
+      label: 'Demo',
+      href: '#',
+    },
+    {
+      label: 'Team',
+      href: '#',
+    },
+    {
+      label: 'Github',
+      href: 'https://github.com/oslabs-beta/Katalog',
+      target: '_blank',
+    },
+  ]
